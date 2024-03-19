6c280d22955a25318a681ce30e1716940e0f5eb2
byte-s <inigam277@gmail.com>
2024-03-19 14:42:29 +0600

    button props fix

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,8 +2,9 @@ import { ButtonProps } from "./Button.props";
 import styles from "./Button.module.css"
 import cn from 'classnames'
 import Link from "next/link";
+import React from "react";
 
-export const Button = ({link, appearance, children}:ButtonProps): JSX.Element => {
+export const Button = ({appearance, children}:ButtonProps): JSX.Element => {
     return (
         <Link href={'/contacts'} className={cn(styles.button, {
             [styles.white]: appearance == 'white',

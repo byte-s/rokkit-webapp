9c30c9ea05a239f63ada45415b1531f532f07148
byte-s <inigam277@gmail.com>
2024-03-25 11:46:48 +0600

    sex

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ function Home(): JSX.Element {
       <Head>
         <link rel="icon" href="../public/favicon.ico" sizes="any" />
         <title>Rokkit - заказать внедрение CRM, разработку сайтов и интернет-магазинов</title>
+        sex
       </Head>
       <CoverHtag>Улучшаем бизнес с помощью уникального плана роста, тщательного анализа рынка и современной разработки, включая внедрение CRM систем</CoverHtag>
       <CallButton appearance='white' arrow>Свяжитесь с нами</CallButton>

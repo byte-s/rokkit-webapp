96f6e90b90c5a5bce813b4fb075506666763fd4d
byte-s <inigam277@gmail.com>
2024-04-02 20:48:56 +0600

    some interfaces fixes

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -75,7 +75,6 @@ export const withAdminLayout = <T extends Record<string, unknown>>(Component: Fu
         return (
             <AdminLayout>
                 <Component {...props}/>
-             
             </AdminLayout>
         )
     }

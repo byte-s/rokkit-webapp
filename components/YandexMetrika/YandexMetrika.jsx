fcc0c322e800e1629c74be03d3dae6aafa241feb
byte-s <inigam277@gmail.com>
2024-04-11 16:15:57 +0600

    script fix

--- components/YandexMetrika/YandexMetrika.jsx
+++ components/YandexMetrika/YandexMetrika.jsx
@@ -2,6 +2,7 @@
  
 import { useEffect } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
+import { Script } from 'next/script'
  
 export function YandexMetrika() {
   const pathname = usePathname()
@@ -14,23 +15,20 @@ useEffect(() => {
  
   return (
     <Script id="metrika-counter" strategy="afterInteractive">
-                        {`
-                        <script type="text/javascript" >
-                        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                        m[i].l=1*new Date();
-                        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-                     
-                        ym(96996731, "init", {
-                            defer:true;
-                             clickmap:true,
-                             trackLinks:true,
-                             accurateTrackBounce:true
-                        });
-                        </script>
-                        <noscript><div><img src="https://mc.yandex.ru/watch/96996731" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-                     `}
-                    </Script>
+      {`
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    
+      ym(96996731, "init", {
+          defer:true;
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+      });
+    `}
+    </Script>
   )
 }
